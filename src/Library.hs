aa8b68eebc9b9778d36f70c898c1c45e2654109f
Juan Manuel Fernandes dos Santos <juan.fernandes@10pines.com>
2020-12-26 08:58:55 -0300

    Update Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -1,2 +1,15 @@
 module Library where
 import PdePreludat
+
+data Carta   = Carta { numero :: Int, palo :: String } deriving (Show, Eq) 
+type Mano    = [Carta]
+data Jugador = Jugador { nombre :: String, mano :: Mano, bebida :: String } deriving (Show, Eq)
+type Mesa    = [Jugador]
+
+palos = ["Corazones", "Picas", "Tréboles", "Diamantes"]
+
+ocurrenciasDe x xs = (length . filter (== x)) xs
+
+mayorSegun f x y
+    | f x > f y = x
+    | otherwise = y
